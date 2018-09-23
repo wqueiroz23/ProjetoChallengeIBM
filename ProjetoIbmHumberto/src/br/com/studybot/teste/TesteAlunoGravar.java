da373ba7f5fa1669ee5c0b5c58f6f92809844921
Wilson Queiroz <w3queiroz@gmail.com>
2018-09-23 16:55:51 -0300

    Commit do gravar aluno atualizado

--- ProjetoIbmHumberto/src/br/com/studybot/teste/TesteAlunoGravar.java
+++ ProjetoIbmHumberto/src/br/com/studybot/teste/TesteAlunoGravar.java
@@ -26,39 +26,13 @@ public class TesteAlunoGravar {
 			a.setSenha("will1566");
 			a.setSexo("M");
 			a.setIdade(28);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			/*a.setCdAluno(Integer.parseInt(JOptionPane.showInputDialog("digite um codigo")));
-			a.setNome(JOptionPane.showInputDialog("Digite seu nome"));
-			a.setApelido(JOptionPane.showInputDialog("Digite um apelido"));
-			a.setCpf(JOptionPane.showInputDialog("digite seu cpf"));
-			a.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade")));
-			a.setEmail(JOptionPane.showInputDialog("Digite seu email"));
-			a.setGrauEscolaridade(JOptionPane.showInputDialog("Digite seu grau de escolaridade"));
-			a.setRg(JOptionPane.showInputDialog("Digite seu rg"));
-			a.setSexo((JOptionPane.showInputDialog("Digite seu sexo")));
-			a.setData(JOptionPane.showInputDialog("Digite sua data de nascimento"));
-			a.setDataFormacao(JOptionPane.showInputDialog("Digite sua data de formação"));
-			a.setHistorico(JOptionPane.showInputDialog("Digite seu historico escolar"));
-			a.setTelefone(Integer.parseInt(JOptionPane.showInputDialog("Digite seu telefone")));
-			a.setSenha(JOptionPane.showInputDialog("digite sua senha"));*/
-			AlunoDAO dao= new AlunoDAO();
+
+			AlunoDAO dao = new AlunoDAO();
 			System.out.println(dao.addAluno(a));
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			
+
 			e.printStackTrace();
 			System.out.println(e);
 		}
